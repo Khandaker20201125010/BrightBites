04a61e71af590151eeba184739686316931b6095
unknown <prantokih42@gmail.com>
2025-01-30 22:56:13 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+       <div className='text-center bg-blue-400'>
+        <h1 className='text-3xl'>hello World</h1>
+        
+       </div>
+    </>
+  )
+}
+
+export default App
