4238952b31d3631aa3cf91863b9ce70f0be20f29
unknown <prantokih42@gmail.com>
2025-02-23 22:48:51 +0600

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <>
        <div className='text-center bg-blue-400'>
         <h1 className='text-3xl'>hello World</h1>
-        
+        <h2>need to work done but im leazy as hell ya ya </h2>
        </div>
     </>
   )
