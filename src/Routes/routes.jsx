1f5ebe8d0f8e9f58f303632a42c7bf30d66a52a6
unknown <prantokih42@gmail.com>
2025-02-24 22:06:21 +0600

    page routes setup

---
+++ src/Routes/routes.jsx
@@ -0,0 +1,46 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import Home from "../Pages/Home";
+import About from "../Pages/About";
+import Appointment from "../Pages/Appointment";
+import ContactUs from "../Pages/ContactUs";
+import Reviews from "../Pages/Reviews";
+import Login from "../Pages/Login";
+import SignUp from "../Pages/SignUp";
+
+export const router = createBrowserRouter([
+{
+    path: '/',
+    element: <Main></Main>,
+    children: [
+        {
+            path: '/',
+            element: <Home></Home>
+        },
+        {
+            path: '/about',
+            element:<About></About> 
+        },
+        {
+            path: '/appointment',
+            element: <Appointment></Appointment>
+        },
+        {
+            path: '/contactUs',
+            element: <ContactUs></ContactUs>
+        },
+        {
+            path: '/reviews',
+            element:<Reviews></Reviews>
+        },
+        {
+            path: '/login',
+            element: <Login></Login>
+        },
+        {
+            path: '/signUp',
+            element: <SignUp></SignUp>
+        },
+    ]
+}
+])
